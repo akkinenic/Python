f1a6f2b3986b82857f00cfafc4d4db43ee16271e
akkinenic <krishnachaitanyaakkineni@gmail.com>
2018-07-20 14:19:14 -0500

    project 1 solution added

---
+++ Ch1Projects.py
@@ -0,0 +1,47 @@
+#29 P-1.29 Write a Python program that outputs all possible strings formed by using
+#the characters c , a , t , d , o , and g exactly once.
+
+#method 1
+
+
+def permute_helper(str,chosen=""):
+    if str == "":
+        print(chosen)
+        return chosen
+    else:
+        for a in str:
+            """ choose"""
+            pull = a
+            chosen +=pull
+            str = str.replace(a,"")
+            """explore"""
+            permute_helper(str,chosen)
+            """un-choose"""
+            str +=pull
+            chosen = chosen.replace(pull,"")
+
+def permute(str):
+    permute_helper(str)
+
+print(permute("catdog"))
+
+#method 2
+
+def permutations(word):
+    if len( word ) == 1:
+        return [ word ]
+
+    # get all permutations of length N-1
+    perms = permutations( word[ 1: ] )
+    char = word[ 0 ]
+    result = [ ]
+    # iterate over all permutations of length N-1
+    for perm in perms:
+        # insert the character into every possible location
+        for i in range( len( perm ) + 1 ):
+            result.append( perm[ :i ] + char + perm[ i: ] )
+
+    return result
+
+
+print(permutations( "catdog" ))
