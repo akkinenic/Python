1f7ce63bb3e958abb9201524cb8a476720d9bb5d
akkinenic <krishnachaitanyaakkineni@gmail.com>
2018-07-21 13:08:15 -0500

    updated solutions

--- Ch2Reinforcement.py
+++ Ch2Reinforcement.py
@@ -59,6 +59,13 @@ class CreditCard:
         self.balance=0
         self.limit=limit
 
+    # def __init__(self, customer, account, bank, limit,balance):
+    #     self.customer = customer
+    #     self.bank = bank
+    #     self.account = account
+    #     self.balance = balance
+    #     self.limit = limit
+
     def get_customer(self):
         return self.customer
 
@@ -90,4 +97,82 @@ class CreditCard:
             raise TypeError( 'amount must be numeric')
         elif amount < 0:
             raise ValueError( 'amount cannot be negative')
-        self.bank -=amount
+        self.balance -=amount
+
+"""
+R-2.6 If the parameter to the make payment method of the CreditCard class
+were a negative number, that would have the effect of raising the balance
+on the account. Revise the implementation so that it raises a ValueError if
+a negative value is sent.
+"""
+
+#ANSWER - REFER TO LINES 96-99
+
+"""
+R-2.7 The CreditCard class of Section 2.3 initializes the balance of a new account
+to zero. Modify that class so that a new account can be given a
+nonzero balance using an optional fifth parameter to the constructor. The
+four-parameter constructor syntax should continue to produce an account
+with zero balance.
+"""
+
+#ANSWER - REFER TO LINES 62-67
+
+"""
+R-2.8 Modify the declaration of the first for loop in the CreditCard tests, from
+Code Fragment 2.3, so that it will eventually cause exactly one of the three
+credit cards to go over its credit limit. Which credit card is it?
+"""
+
+
+#if __name__ == __main__ :
+wallet = [ ]
+wallet.append(CreditCard( 'John Bowman ', 5391037593875309, 'California Savings'  , 2500) )
+wallet.append(CreditCard( 'John Bowman ', 3485039933951954, 'California Federal'   , 3500) )
+wallet.append(CreditCard( 'John Bowman' ,5391037593875309, 'California Finance',  5000) )
+
+for val in range(1, 17):
+    wallet[0].charge(val)
+    wallet[1].charge(2*val)
+    wallet[2].charge(3*val)
+
+for c in range(3):
+    print( "Customer = ", wallet[c].get_customer())
+    print( "Bank = ", wallet[c].get_bank())
+    print( "Account =" , wallet[c].get_account( ))
+    print( "Limit = ", wallet[c].get_limit( ))
+    print( "Balance =" , wallet[c].get_balance())
+
+while wallet[c].get_balance() > 100:
+    wallet[c].make_payment(100)
+    print( "New balance = ", wallet[c].get_balance())
+    print( )
+
+
+"""
+R-2.9 Implement the sub method for the Vector class of Section 2.3.3, so
+that the expression u−v returns a new vector instance representing the
+difference between two vectors."""
+
+
+def sub(self, other):
+    if len(self) != len(other): # relies on len method
+        raise ValueError( "dimensions must agree ")
+        #result = Vector(len(self)) # start with vector of zeros
+    for j in range(len(self)):
+        #result[j] = self[j] - other[j]
+        #return result
+
+"""
+R-2.10 Implement the neg method for the Vector class of Section 2.3.3, so
+that the expression −v returns a new vector instance whose coordinates
+are all the negated values of the respective coordinates of v.
+"""
+
+# commented the code below because of IDE
+
+#def neg(self):
+    # result = Vector(len(self)) # start with vector of zeros
+    # for j in range(len(self)):
+    #     result[j] = ~self[j]
+    #     return result
