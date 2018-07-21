0c6e362f90807da283e26e0b84a240dacc9dfb86
akkinenic <krishnachaitanyaakkineni@gmail.com>
2018-07-21 10:46:01 -0500

    updated solutions

--- Ch2Reinforcement.py
+++ Ch2Reinforcement.py
@@ -42,3 +42,52 @@ class Flower:
 flower = Flower("Rose",20,5.6)
 
 print(flower.get_flower())
+
+
+"""
+R-2.5 Use the techniques of Section 1.7 to revise the charge and make payment
+methods of the CreditCard class to ensure that the caller sends a number
+as a parameter.
+"""
+
+
+class CreditCard:
+    def __init__(self,customer,account,bank,limit):
+        self.customer = customer
+        self.bank=bank
+        self.account=account
+        self.balance=0
+        self.limit=limit
+
+    def get_customer(self):
+        return self.customer
+
+    def get_balance(self):
+        return self.balance
+
+    def get_account(self):
+        return self.account
+
+    def get_bank(self):
+        return self.bank
+
+    def get_limit(self):
+        return self.limit
+
+    def charge(self,price):
+        if not isinstance( price, (int, float) ):
+            raise TypeError( 'price must be numeric')
+        elif price < 0:
+            raise ValueError( 'price cannot be negative')
+        if price + self.balance > self.limit:  # if charge would exceed limit,
+            return False  # cannot accept charge
+        else:
+            self.balance += price
+        return True
+
+    def make_payment( self,amount ):
+        if not isinstance( amount, (int, float) ):
+            raise TypeError( 'amount must be numeric')
+        elif amount < 0:
+            raise ValueError( 'amount cannot be negative')
+        self.bank -=amount
