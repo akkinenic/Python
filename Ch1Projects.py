8fbe10482a95d95fb73f43ef050036c38083195c
akkinenic <krishnachaitanyaakkineni@gmail.com>
2018-07-20 20:26:39 -0500

    updated solutions

--- Ch1Projects.py
+++ Ch1Projects.py
@@ -151,6 +151,7 @@ again.” Your program should number each of the sentences and it should
 make eight different random-looking typos.
 """
 
+
 def punishment():
     content = {}
     for i in range(1,101):
@@ -161,4 +162,64 @@ def punishment():
     from pprint import pprint
     pprint(content)
 
-punishment()
+
+#punishment()
+
+
+"""
+P-1.35 The birthday paradox says that the probability that two people in a room
+will have the same birthday is more than half, provided n, the number of
+people in the room, is more than 23. This property is not really a paradox,
+but many people find it surprising. Design a Python program that can test
+this paradox by a series of experiments on randomly generated birthdays,
+which test this paradox for n = 5,10,15,20, . . . ,100.
+"""
+
+
+def test_birthday(n):
+    from random import randint
+    from itertools import groupby
+    random_birthdays = [randint(1,31) for ele in range(n)]
+    print(random_birthdays)
+    frequency_ = [ len( list( group ) ) for key, group in groupby( random_birthdays ) ]
+    count = 0
+    for ele in frequency_:
+        if ele > 1:
+            count += 1
+    return "Two people in a room have same birthday" if count > 0 else "Two people in a room do not have same birthday"
+
+
+print("5 people group -->"+ str(test_birthday(5)))
+print("5 people group -->"+ str(test_birthday(10)))
+print("5 people group -->"+ str(test_birthday(15)))
+print("5 people group -->"+ str(test_birthday(20)))
+print("5 people group -->"+ str(test_birthday(25)))
+print("5 people group -->"+ str(test_birthday(30)))
+print("5 people group -->"+ str(test_birthday(35)))
+print("5 people group -->"+ str(test_birthday(40)))
+print("5 people group -->"+ str(test_birthday(45)))
+print("5 people group -->"+ str(test_birthday(50)))
+print("5 people group -->"+ str(test_birthday(55)))
+
+
+"""
+P-1.36 Write a Python program that inputs a list of words, separated by whitespace,
+and outputs how many times each word appears in the list. You
+need not worry about efficiency at this point, however, as this topic is
+something that will be addressed later in this book.
+"""
+
+
+def word_counter():
+    from collections import Counter
+    data = input("Input the list of words\n").split(" ")
+    content = {}
+    for a in data:
+        if content.__contains__(a):
+            content[a] +=1
+        else:
+            content[a] = 1
+    from pprint import pprint
+    pprint(content)
+
+word_counter()
